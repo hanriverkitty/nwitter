7262af03380d270b25f97b2522d481fd10830ce0
hanriverkitty <danieltls@naver.com>
2022-07-31 09:49:06 +0900

    gitignore4

--- nwitter/src/firebase.js
+++ nwitter/src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC8WYqQmnSSVV_NURW9mYCrv6Sg0xCZRjQ",
-    authDomain: "nwitter-69b57.firebaseapp.com",
-    projectId: "nwitter-69b57",
-    storageBucket: "nwitter-69b57.appspot.com",
-    messagingSenderId: "616196257184",
-    appId: "1:616196257184:web:689cc797ba7b262be9e0c5"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
